5565a0312127327aa4e655b747dd159413de61cd
dimakisiv <dimakisiv@gmail.com>
2026-04-10 13:00:03 +0300

    added encoding for S3 bucket

--- lesson-5/modules/s3-backend/s3.tf
+++ lesson-5/modules/s3-backend/s3.tf
@@ -17,6 +17,17 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   }
 }
 
+# Вмикаємо серверне шифрування за замовчуванням для S3-бакета
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_sse" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 # Встановлюємо контроль власності для S3-бакета
 resource "aws_s3_bucket_ownership_controls" "terraform_state_ownership" {
   bucket = aws_s3_bucket.terraform_state.id
