9596d0724d119b31b501905b95017dd3bc26b2d5
dimakisiv <dimakisiv@gmail.com>
2026-04-10 13:16:29 +0300

    added aws provider version

--- lesson-5/backend.tf
+++ lesson-5/backend.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.38.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "terraform-state-bucket-dk-lesson-5" # Назва S3-бакета
     key            = "lesson-5/terraform.tfstate"   # Шлях до файлу стейту
