5a179033441d56274ed47fafdd2f342dfff034d2
dimakisiv <dimakisiv@gmail.com>
2026-04-20 20:26:54 +0300

    updated name for s3 bucket backend

--- lesson-8-9/backend.tf
+++ lesson-8-9/backend.tf
@@ -13,8 +13,8 @@ terraform {
 	}
 	
 	backend "s3" {
-		bucket         = "terraform-state-bucket-dk-7" # Назва S3-бакета
-		key            = "lesson-7/terraform.tfstate"   # Шлях до файлу стейту
+		bucket         = "terraform-state-bucket-dk-8-9" # Назва S3-бакета
+		key            = "lesson-8-9/terraform.tfstate"   # Шлях до файлу стейту
 		region         = "eu-north-1"                    # Регіон AWS
 		dynamodb_table = "terraform-locks"              # Назва таблиці DynamoDB для блокування
 		encrypt        = true                           # Шифрування файлу стейту
